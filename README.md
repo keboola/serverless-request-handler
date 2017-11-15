0c239b0f352c4510d0577c5ce422ba3452f55d98
Jakub Matejka <jakub@keboola.com>
2017-11-15 17:18:40 +0100

    :zap: Init

---
+++ README.md
@@ -0,0 +1,15 @@
+## serverless-request-handler
+
+[![serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
+[![Build Status](https://travis-ci.org/keboola/serverless-request-handler.svg?branch=master)](https://travis-ci.org/keboola/serverless-request-handler)
+
+Wrapper for lambda functions creating unified response for error states. 
+
+
+### Installation
+
+1. Install npm package: `yarn add @keboola/serverless-request-handler`
+2. Wrap each of your lambda handlers with this code:
+```js
+
+```
---
+++ src/index.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const _ = require('lodash');
+
+class UserError extends Error {
+  constructor(...args) {
+    super(...args);
+    Error.captureStackTrace(this, UserError);
+  }
+
+  static badRequest(msg = 'Bad Request') {
+    const err = new UserError(msg);
+    err.code = 400;
+    err.type = 'BadRequest';
+    return err;
+  }
+
+  static unauthorized(msg = 'Unauthorized') {
+    const err = new UserError(msg);
+    err.code = 401;
+    err.type = 'Unauthorized';
+    return err;
+  }
+
+  static notFound(msg = 'Not Found') {
+    const err = new UserError(msg);
+    err.code = 404;
+    return err;
+  }
+
+  static unprocessable(msg = 'Unprocessable') {
+    const err = new UserError(msg);
+    err.code = 422;
+    return err;
+  }
+
+  static error(msg = 'Error', code = 400) {
+    const err = new UserError(msg);
+    err.code = code;
+    return err;
+  }
+}
+
+class RequestHandler {
+  /**
+   * Logs API request to console
+   */
+  static logRequest(context, err, event, res = null) {
+    const log = {
+      event: {
+        requestId: context.awsRequestId,
+        apiRequestId: event.requestContext.requestId,
+        function: context.functionName,
+        httpMethod: event.httpMethod,
+        path: event.path,
+      },
+    };
+    if (res && 'statusCode' in res) {
+      log.statusCode = res.statusCode;
+    }
+    if (err) {
+      log.error = err.message;
+    }
+
+    console.log(JSON.stringify(log));
+  }
+
+  /**
+   * Encapsulates each lambda handler and handles uncaught user errors
+   */
+  static handler(fn, event, context, cb) {
+    try {
+      fn();
+    } catch (err) {
+      if (!(err instanceof UserError)) {
+        throw err;
+      }
+      const res = RequestHandler.getResponseBody(err, null, context);
+      if (_.isObject(res.body)) {
+        res.body = JSON.stringify(res.body);
+      }
+      RequestHandler.logRequest(context, err, event, res);
+      return cb(null, res);
+    }
+  }
+
+  /**
+   * Formats response for API
+   */
+  static getResponseBody(err, res, context, code = 200) {
+    const response = {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      statusCode: code,
+      body: res,
+    };
+    if (err) {
+      if (!(err instanceof UserError)) {
+        throw err;
+      }
+      response.statusCode = _.isNumber(err.code) ? err.code : 400;
+      response.body = {
+        errorMessage: err.message,
+        errorType: err.type,
+        requestId: context.awsRequestId,
+      };
+    }
+
+    return response;
+  }
+
+  /**
+   * Returns result of promise chain to API and handles rejection
+   */
+  static responsePromise(promise, event, context, callback, code = 200) {
+    return promise
+      .then(res =>
+        RequestHandler.response(null, res, event, context, callback, code)
+      )
+      .catch(err =>
+        RequestHandler.response(err, null, event, context, callback)
+      );
+  }
+
+  static response(err, res, event, context, cb, code = 200) {
+    const response = RequestHandler.getResponseBody(err, res, context, code);
+
+    RequestHandler.logRequest(context, err, event, response);
+
+    response.body = response.body ? JSON.stringify(response.body) : '';
+    cb((err instanceof UserError) ? null : err, response);
+  }
+}
+
+module.exports = { RequestHandler, UserError };
